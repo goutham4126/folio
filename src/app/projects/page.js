eeae7e178bcf455dc234c0de3b2ec94f1ed1f539
goutham4126 <goutham4126@gmail.com>
2024-10-11 07:12:04 +0530

    changes

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -47,8 +47,8 @@ function Projects() {
   ];
 
   return (
-    <div className="p-5 bg-gray-50 text-gray-800 pt-10">
-      <h1 className="text-xl md:text-3xl font-bold text-center text-slate-700">✨ Have a look at my Work</h1>
+    <div className="p-5 text-gray-800">
+      <h1 className="text-xl md:text-3xl font-bold text-center text-slate-300">✨ Have a look at my Work</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8 mt-8">
         {projects.map((project, index) => (
           <div
